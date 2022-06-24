09e5f0eb6600f9e0fbf3735c9052f344f450fed1
minsoozz <minsoo1604@naver.com>
2022-06-24 23:08:00 +0900

    feat: RestTemplate Bean 수정

--- src/main/java/com/exchange/calculator/config/RestTemplateBean.java
+++ src/main/java/com/exchange/calculator/config/RestTemplateBean.java
@@ -1,15 +1,37 @@
 package com.exchange.calculator.config;
 
 
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
+
 
 @Configuration
 public class RestTemplateBean {
 
   @Bean
-  public RestTemplate restTemplate(){
-    return new RestTemplate();
+  public RestTemplate restTemplate() {
+    HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory();
+    requestFactory.setHttpClient(httpClient());
+    return new RestTemplate(requestFactory);
+  }
+
+  @Bean
+  public HttpComponentsClientHttpRequestFactory httpComponentsClientHttpRequestFactory() {
+    HttpComponentsClientHttpRequestFactory httpComponentsClientHttpRequestFactory = new HttpComponentsClientHttpRequestFactory();
+    httpComponentsClientHttpRequestFactory.setConnectTimeout(2000);
+    httpComponentsClientHttpRequestFactory.setReadTimeout(5000);
+    return httpComponentsClientHttpRequestFactory;
+  }
+
+  @Bean
+  public CloseableHttpClient httpClient() {
+    return HttpClientBuilder.create()
+        .setMaxConnTotal(100)
+        .setMaxConnPerRoute(5)
+        .build();
   }
 }
