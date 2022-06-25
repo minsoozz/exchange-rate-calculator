8984d685740d3619f4f847b3ac176b91dd0e6d59
minsoozz <minsoo1604@naver.com>
2022-06-25 21:35:01 +0900

    feat: 계산 결과 반환 기능 추가

--- src/main/java/com/exchange/calculator/controller/CalculatorRestController.java
+++ src/main/java/com/exchange/calculator/controller/CalculatorRestController.java
@@ -1,22 +1,29 @@
 package com.exchange.calculator.controller;
 
+import com.exchange.calculator.service.CalculatorService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping("/api")
 @RestController
-public class calculatorRestController {
+@RequestMapping("/api/calculators")
+public class CalculatorRestController {
 
   private final CalculatorService calculatorService;
 
-  public calculatorRestController(CalculatorService calculatorService) {
+  public CalculatorRestController(CalculatorService calculatorService) {
     this.calculatorService = calculatorService;
   }
 
   @GetMapping
-  public String get() {
-    calculatorService.get();
-    return "get";
+  public ResponseEntity<Double> get(@RequestParam String country) {
+    return ResponseEntity.ok(calculatorService.getExchangeRateByCountry(country));
+  }
+
+  @GetMapping("/exchange")
+  public ResponseEntity<String> exchange(@RequestParam Double exchangeRate, @RequestParam Double remittanceAmount){
+    return ResponseEntity.ok(calculatorService.exchangeCalculator(exchangeRate,remittanceAmount));
   }
 }
